410cf0dc571366551208012d47f943328618e4c9
Saravanan Sathiyamoorthi <developer.saro@gmail.com>
2021-11-17 17:20:27 +0530

    Add files via upload

---
+++ compare/comaprison.js
@@ -0,0 +1,155 @@
+let graph_container = document.querySelector(".graph-container");
+let graphMainContainer = document.querySelector("#graph-main");
+let multiGraphMainContainer = document.querySelector("#multi-graph-main");
+
+let fields;
+function compare_algos() {
+  Bubble("Bubble");
+  Selection_sort("Selection");
+  Insertion("Insertion");
+  Merge("Merge");
+  Quick("Quick");
+  console.log("Comparison data", vis_data);
+  fields = Object.keys(vis_data);
+  comparison_graph();
+}
+// elements[0].onload = console.log(vis_data);
+function getGroupedData() {
+  let datagroup = [];
+  fields.forEach((field) => {
+    let time = [];
+    Object.keys(vis_data[field]).forEach((size) => {
+      time = [...time, vis_data[field][size]["time"]];
+    });
+    datagroup = [...datagroup, time];
+  });
+  // console.log("Grouped Array", datagroup);
+  return datagroup;
+}
+
+function createGraphContainers() {
+  graph_container.innerHTML = "";
+  fields.forEach((data, index) => {
+    graph_container.innerHTML += `<div>
+        <canvas class="myChart" id="chart-${index}"></canvas>
+        <span>${data}</span>
+    </div>`;
+  });
+}
+
+function createComparisonGraphContainer() {
+  // if (document.querySelector("#multi-plot") === undefined)
+  multiGraphMainContainer.innerHTML = "";
+  multiGraphMainContainer.innerHTML = `<div class="comapare-graphs-container">
+    <canvas class="myChart" id="multi-plot"></canvas>
+  </div>`;
+}
+function comparison_graph() {
+  createGraphContainers();
+  createComparisonGraphContainer();
+  //   let elements = document.querySelectorAll(".myChart");
+
+  let groupedData = getGroupedData();
+  fields.forEach((e, index) => {
+    let data = [],
+      labels = [],
+      type = "line",
+      target = document.querySelector(`#chart-${index}`);
+    Object.keys(vis_data[e]).forEach((size) => {
+      labels = [...labels, size];
+      data = [...data, vis_data[e][size]["time"]];
+    }),
+      console.log(labels, data);
+
+    graph(target, type, labels, data);
+  });
+  let multiplotContainer = document.querySelector("#multi-plot");
+  multiGraph(multiplotContainer, "line", fields, groupedData);
+  console.log("data", groupedData);
+  // groupedData();
+}
+
+function graph(target, type, labels, data) {
+  const chart = new Chart(target, {
+    type: type,
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: "Sample plot",
+          data: data,
+          backgroundColor: `rgb(${Math.random() * 255},${Math.random() * 255},${
+            Math.random() * 255
+          })`,
+
+          borderColor: `rgb(${Math.random() * 255},${Math.random() * 255},${
+            Math.random() * 255
+          })`,
+        },
+      ],
+    },
+  });
+}
+
+function multiGraph(target, type, labels, data) {
+  let datasets = [];
+  labels.forEach((field, index) => {
+    datasets = [
+      ...datasets,
+      {
+        label: field,
+        data: data[index],
+        backgroundColor: `rgb(${Math.random() * 255},${Math.random() * 255},${
+          Math.random() * 255
+        })`,
+        borderColor: `rgb(${Math.random() * 255},${Math.random() * 255},${
+          Math.random() * 255
+        })`,
+      },
+    ];
+  });
+  const chart = new Chart(target, {
+    type: type,
+    data: {
+      labels: labels,
+      datasets: datasets,
+    },
+    options: {
+      responsive: true,
+      interaction: {
+        mode: "index",
+        intersect: false,
+      },
+      stacked: false,
+      plugins: {
+        title: {
+          display: true,
+          text: "Chart.js Line Chart - Multi Axis",
+        },
+      },
+      scales: {
+        y: {
+          type: "linear",
+          display: true,
+          position: "left",
+        },
+        y1: {
+          type: "linear",
+          display: true,
+          position: "right",
+
+          // grid line settings
+          grid: {
+            drawOnChartArea: false, // only want the grid lines for one axis to show up
+          },
+        },
+      },
+    },
+  });
+}
+
+compare_btn.addEventListener("click", () => {
+  visualization_container.style.display = "none";
+  graph_main_container.style.display = "block";
+  compare_algos();
+});
---
+++ compare/main.js
@@ -0,0 +1,95 @@
+//Variables (BE CAREFUL THESE MIGHT BE USED IN OTHER JS FILES TOO)
+var inp_as = document.getElementById("a_size"),
+  array_size = inp_as.value;
+var inp_gen = document.getElementById("a_generate");
+var inp_aspeed = document.getElementById("a_speed");
+//var array_speed=document.getElementById('a_speed').value;
+var compare_btn = document.querySelector("#compare-btn");
+var butts_algos = document.querySelectorAll(".algos button");
+let visualization_container = document.querySelector("#visualization");
+let graph_main_container = document.querySelector("#graph-section");
+var div_sizes = [];
+var divs = [];
+var margin_size;
+var cont = document.getElementById("array_container");
+cont.style = "flex-direction:row";
+
+let vis_data = {};
+//Array generation and updation.
+
+inp_gen.addEventListener("click", generate_array);
+inp_as.addEventListener("change", update_array_size);
+
+function generate_array() {
+  cont.innerHTML = "";
+  visualization_container.style.display = "block";
+  graph_main_container.style.display = "none";
+  for (var i = 0; i < array_size; i++) {
+    div_sizes[i] =
+      Math.floor(Math.random() * 0.5 * (inp_as.max - inp_as.min)) + 10;
+    divs[i] = document.createElement("div");
+    cont.appendChild(divs[i]);
+    margin_size = 0.1;
+    divs[i].style =
+      " margin:0% " +
+      margin_size +
+      "%; background-color:rgb(255,12,147); width:" +
+      (100 / array_size - 2 * margin_size) +
+      "%; height:" +
+      div_sizes[i] * 0.42 +
+      "%;";
+  }
+
+  //   vis_data = {
+  //     ...vis_data,
+  //     [array_size]: { array: div_sizes.slice(0, array_size), time: {} },
+  //   };
+  //   console.log("array data", vis_data);
+}
+
+function update_array_size() {
+  array_size = inp_as.value;
+  generate_array();
+}
+
+window.onload = update_array_size();
+
+//Running the appropriate algorithm.
+for (var i = 0; i < butts_algos.length; i++) {
+  butts_algos[i].addEventListener("click", runalgo);
+}
+
+function disable_buttons() {
+  for (var i = 0; i < butts_algos.length; i++) {
+    butts_algos[i].classList = [];
+    butts_algos[i].classList.add("butt_locked");
+
+    butts_algos[i].disabled = true;
+    inp_as.disabled = true;
+    inp_gen.disabled = true;
+    inp_aspeed.disabled = true;
+  }
+}
+
+function runalgo() {
+  disable_buttons();
+  let selected = this.innerHTML;
+  this.classList.add("butt_selected");
+  switch (selected) {
+    case "Bubble":
+      Bubble(selected);
+      break;
+    case "Selection":
+      Selection_sort(selected);
+      break;
+    case "Insertion":
+      Insertion(selected);
+      break;
+    case "Merge":
+      Merge(selected);
+      break;
+    case "Quick":
+      Quick(selected);
+      break;
+  }
+}
